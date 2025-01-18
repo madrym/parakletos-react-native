71390b238f48f4de57482854aff6b4e35b46363c
Michael Madry <michael_madry@hotmail.com>
2025-01-18 22:56:57 +1100

    init: auth + basic setup

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
-const Index = () => {
+const Home = () => {
   return (
     <View>
-      <Text>Index</Text>
+      <Text>Home</Text>
     </View>
   );
 };
 
-export default Index;
+export default Home;
